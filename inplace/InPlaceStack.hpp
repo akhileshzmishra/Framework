d8d30e8edf76038ffc985b55ec78264a3e697242
akhilesh mishra <akhileshzmishra@gmail.com>
2025-12-31 23:19:02 +0000

    Refine Inplace stack

--- inplace/InPlaceStack.hpp
+++ inplace/InPlaceStack.hpp
@@ -2,62 +2,66 @@
 #define INPLACESTACK_HPP
 #include <array>
 namespace inplace {
-    /**
-     * @brief 
-     * @tparam T
-     * @tparam Size 
-     */
-    template<class T, int Size>
-    class InPlaceStack {
-    public:
-        InPlaceStack();
 
-        ~InPlaceStack() = default;
+template <typename T>
+concept StackableLike = std::is_nothrow_default_constructible_v<T>;
 
-        void Push(const T &);
+template <StackableLike T, int Size>
+class InPlaceStack {
+   public:
+    InPlaceStack ();
 
-        void Pop();
+    ~InPlaceStack () = default;
 
-        T &Top();
+    template <class UT>
+        requires std::is_same_v<T, UT>
+    void Push (UT &&);
 
-        [[nodiscard]] bool Empty() const;
+    void Pop ();
 
-        [[nodiscard]] bool Full() const;
+    T &Top ();
 
-    private:
-        std::array<T, Size> stack;
-        int d_top;
-        int d_size;
-    };
+    [[nodiscard]] bool Empty () const;
 
-    template<class T, int Size>
-    InPlaceStack<T, Size>::InPlaceStack(): d_top(0), d_size(0) {
-    }
+    [[nodiscard]] bool Full () const;
 
-    template<class T, int Size>
-    void InPlaceStack<T, Size>::Push(const T &data) {
-        stack[d_top] = data;
-        d_top++;
-    }
+   private:
+    using Cell = std::aligned_storage_t<sizeof (T), alignof (T)>;
+    std::array<Cell, Size> stack;
+    int d_top;
+    int d_size;
+};
 
-    template<class T, int Size>
-    void InPlaceStack<T, Size>::Pop() {
-        d_top--;
-    }
+template <StackableLike T, int Size>
+InPlaceStack<T, Size>::InPlaceStack () : d_top (0), d_size (0) {}
 
-    template<class T, int Size>
-    T &InPlaceStack<T, Size>::Top() {
-        return stack[d_top];
-    }
-
-    template<class T, int Size>
-    bool InPlaceStack<T, Size>::Empty() const {
-        return d_top == 0;
-    }
-
-    template<class T, int Size>
-    bool InPlaceStack<T, Size>::Full() const {
-        return d_top == Size;
-    }
+template <StackableLike T, int Size>
+template <class UT>
+    requires std::is_same_v<T, UT>
+void InPlaceStack<T, Size>::Push (UT &&ut) {
+    new (stack[d_top]) UT (std::forward<UT> (ut));
+    d_top++;
 }
+
+template <StackableLike T, int Size>
+void InPlaceStack<T, Size>::Pop () {
+    stack[d_top--].~T ();
+    d_top--;
+}
+
+template <StackableLike T, int Size>
+auto InPlaceStack<T, Size>::Top () -> T & {
+    return std::launder (stack[d_top]);
+}
+
+template <StackableLike T, int Size>
+bool InPlaceStack<T, Size>::Empty () const {
+    return d_top == 0;
+}
+
+template <StackableLike T, int Size>
+bool InPlaceStack<T, Size>::Full () const {
+    return d_top == Size;
+}
+}  // namespace inplace
 #endif //INPLACESTACK_HPP
